80cccb2f34f8321a16fb1dba7cf310ce9451b67a
stevejhiggs <stevejhiggs@hotmail.com>
2020-10-25 14:49:52 +0000

    start adding json schema validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import health from './health';
+import schemaTest from './schemaTest';
 
 export default function registerRoutes(app: FastifyInstance) {
   health(app);
+  schemaTest(app);
 }
