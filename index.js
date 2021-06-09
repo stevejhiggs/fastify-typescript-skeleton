6a92d16fa30584b473475637b463dc9a4abc98be
Steve Higgs <stevejhiggs@hotmail.com>
2021-06-09 11:27:41 +0100

    upgrade packages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable unicorn/prefer-module */
 require('ts-node').register(); // teach node about typescript then require in the rest of the app
 require('./src/listener');
