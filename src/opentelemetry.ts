c92cdd086f606462be3364360b29acceaac4ee59
steve higgs <stevejhiggs@hotmail.com>
2021-06-25 13:26:00 +0100

    zipkin integration

--- src/opentelemetry.ts
+++ src/opentelemetry.ts
@@ -1,14 +1,25 @@
-import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { NodeTracerProvider } from '@opentelemetry/node';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { ExporterConfig, ZipkinExporter } from '@opentelemetry/exporter-zipkin';
 
 const provider = new NodeTracerProvider();
 
-provider.addSpanProcessor(
-  // Note: SimpleSpanProcessor should not be used in production (use BatchSpanProcessor instead).
-  new SimpleSpanProcessor(new ConsoleSpanExporter())
-);
+// Add your zipkin url (`http://localhost:9411/api/v2/spans` is used as
+// default) and application name to the Zipkin options.
+// You can also define your custom headers which will be added automatically.
+const zipkinOptions: ExporterConfig = {
+  headers: {
+    'my-header': 'header-value'
+  },
+  url: 'http://localhost:9411/api/v2/spans',
+  serviceName: 'fastify-example'
+};
+
+const exporter = new ZipkinExporter(zipkinOptions);
+
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
 provider.register();
 
--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,6 +2,19 @@ import { FastifyInstance } from 'fastify';
 
 export default function registerRoutes(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
+    //custom trace
+    const {
+      activeSpan,
+      tracer
+      // context,
+      // extract,
+      // inject,
+    } = request.openTelemetry();
+
+    const childSpan = tracer.startSpan(`${(activeSpan as any).name} - child process`);
+    // doSomeWork()
+    childSpan.end();
+
     reply.send('OK');
   });
 }
