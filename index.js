4bbd70081834a990a15b78aba672f3209b864206
Steve Higgs <stevejhiggs@hotmail.com>
2022-06-11 17:42:03 +0100

    use esbuild in dev mode for a massive startup speed boost

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable unicorn/prefer-module */
-require('ts-node').register(); // teach node about typescript then require in the rest of the app
+require('esbuild-register/dist/node').register(); // teach node about typescript then require in the rest of the app
 require('./src/listener');
