4b480de782b756f268310e4b3e27746645df6994
Steve Higgs <stevejhiggs@hotmail.com>
2022-06-11 17:58:44 +0100

    switch to swc rather than esbuild

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable unicorn/prefer-module */
-require('esbuild-register/dist/node').register(); // teach node about typescript then require in the rest of the app
+require('@swc-node/register'); // teach node about typescript then require in the rest of the app
 require('./src/listener');
