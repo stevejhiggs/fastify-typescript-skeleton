4f256ef1e7ded37a018c715eac9ee064ae8449fe
Steve Higgs <steve@veed.io>
2022-10-15 19:05:44 +0100

    upgrade deps and take into account the swagger-ui split

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,27 @@
 import fastify from 'fastify';
 import helmet from '@fastify/helmet';
 import fastifySwagger, { SwaggerOptions } from '@fastify/swagger';
-import { ajvTypeBoxPlugin, TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import { TypeBoxTypeProvider, TypeBoxValidatorCompiler } from '@fastify/type-provider-typebox';
 
 import registerRoutes from './routes';
 import { EnhancedFastifyInstance } from './types';
 
 export default async function getServer(port = 3000) {
   const app = fastify({
-    ajv: {
-      plugins: [ajvTypeBoxPlugin]
-    },
     ignoreTrailingSlash: true,
     logger: {
       level: process.env.LOG_LEVEL || 'info'
     }
-  }).withTypeProvider<TypeBoxTypeProvider>();
+  })
+    .withTypeProvider<TypeBoxTypeProvider>()
+    .setValidatorCompiler(TypeBoxValidatorCompiler);
 
   // add security headers
   await app.register(helmet);
 
   // adds open api documentations at /documentation
-  if (process.env.DISABLE_DOCS !== 'true')
+  if (process.env.DISABLE_DOCS !== 'true') {
     await app.register(fastifySwagger, {
       swagger: {
         info: {
@@ -30,9 +30,17 @@ export default async function getServer(port = 3000) {
         },
         host: `localhost:${port}`
       },
-      staticCSP: true,
       exposeRoute: true
     } as SwaggerOptions);
+    await app.register(fastifySwaggerUi, {
+      routePrefix: '/documentation',
+      uiConfig: {
+        docExpansion: 'full',
+        deepLinking: false
+      },
+      staticCSP: true
+    });
+  }
 
   registerRoutes(app as EnhancedFastifyInstance);
 
