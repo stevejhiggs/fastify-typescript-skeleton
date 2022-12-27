324b005871a1a16fdc5719a6b0c4ec2597e1a6e6
Steve Higgs <steve@veed.io>
2022-12-27 10:39:13 +0000

    directly call the listener on start

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// this file is here to make debugging simpler
+
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable unicorn/prefer-module */
 const { register } = require('esbuild-register/dist/node'); // teach node about typescript then require in the rest of the app
