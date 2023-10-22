93ca957a9bf85a7574474b46d676e81eba3853af
Steve higgs <stevejhiggs@hotmail.com>
2023-10-22 12:58:04 +0100

    switch back to ts-node
    esbuild-register has trouble with native modules + node >= 20.6.0

--- index.js
+++
@@ -1,7 +0,0 @@
-// teach node about typescript by using esbuild then require in the rest of the app
-
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable unicorn/prefer-module */
-const { register } = require('esbuild-register/dist/node');
-register();
-require('./src/listener');
---
+++ index.ts
@@ -0,0 +1,1 @@
+import './src/listener.js';
