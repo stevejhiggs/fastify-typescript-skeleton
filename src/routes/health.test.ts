755433fd48158bd08869c98416e2d227d4c20d3b
Steve Higgs <steve@veed.io>
2023-11-26 16:21:41 +0000

    switch to the node native test runner

--- src/routes/health.test.ts
+++ src/routes/health.test.ts
@@ -1,4 +1,5 @@
-import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, it } from 'node:test';
+import assert from 'node:assert';
 
 import getServer from '../server.js';
 import { EnhancedFastifyInstance } from '../types.js';
@@ -23,7 +24,7 @@ describe('endpoints -> health', () => {
         url: '/health'
       });
 
-      expect(result.statusCode).toBe(200);
+      assert.strictEqual(result.statusCode, 200);
     });
 
     it('should return a 404 with a POST request', async () => {
@@ -32,7 +33,7 @@ describe('endpoints -> health', () => {
         url: '/health'
       });
 
-      expect(result.statusCode).toBe(404);
+      assert.strictEqual(result.statusCode, 404);
     });
   });
 });
